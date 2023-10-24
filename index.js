3f4e2f8a6c05806b8380614e50fe67ddf3d4a4b2
Preet <ps440939@gmail.com>
2023-10-24 12:32:46 +0530

    portfolio

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const User = require('./model/User');
+var cors = require('cors');
+
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+const port = process.env.PORT || 5000;
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+    credentials: true,
+    origin: "https://dilpreetsinghportfolio.onrender.com"
+}))
+
+
+app.post('/', async (req, res) => {
+
+    const { username, email, message } = req.body;
+    try {
+        const userDoc = await User.create({ username, email, message });
+        res.json({ msg: "Successfully submitted" });
+    }
+    catch (error) {
+        res.status(400).json({ msg: "Email already registered" });
+    }
+});
+app.get('/dashboard', async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).json({ message: "Successful" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error retrieving data' });
+    }
+})
+const start = async () => {
+    try {
+        await mongoose.connect(mongoURI).then(response => {
+            console.log('Server connection successful');
+        }).catch(err => {
+            console.error("Error connecting to the database");
+        });
+        app.listen(port, () => {
+            console.log("server is running..")
+        })
+    } catch (error) {
+        console.error("Error connecting to the database");
+    }
+}
+start();
